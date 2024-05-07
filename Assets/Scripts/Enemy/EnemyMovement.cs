24d702c5a797b9bfc05b85e1d321fe2cf5b3f6df
Efef-e <emirhanefe458@gmail.com>
2024-05-08 00:21:44 +0300

    Enemy Patrolling

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -11,18 +11,37 @@ public class EnemyMovement : MonoBehaviour
 
     public bool isStatic;
     public bool isWalker;
+    public bool isPatroller;
+    public bool waiter;
+    public float waitTime = 1;
+    bool isWaiting;
     public bool isWalkingRight;
+
+    public Transform wallCheck, groundCheck, gapCheck;
+    public bool wallDetected, groundDetected, gapDetected;
+    public float detectionRadius;
+    public LayerMask whatIsGround;
+    public Transform pointA, pointB;
+    bool moveToA, moveToB;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        moveToA = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        gapDetected = !Physics2D.OverlapCircle(gapCheck.position, detectionRadius, whatIsGround);
+        wallDetected = Physics2D.OverlapCircle(wallCheck.position, detectionRadius, whatIsGround);
+        groundDetected = Physics2D.OverlapCircle(groundCheck.position, detectionRadius, whatIsGround);
 
+        if ((gapDetected || wallDetected && groundDetected))
+        {
+            Flip();
+        }
     }
 
     private void FixedUpdate()
@@ -45,5 +64,70 @@ public class EnemyMovement : MonoBehaviour
                 rb.velocity = new Vector2(speed * Time.deltaTime, rb.velocity.y);
             }
         }
+        if (isPatroller)
+        {
+
+            if (moveToA)
+            {
+                if (!isWaiting)
+                {
+                    rb.velocity = new Vector2(-speed * Time.deltaTime, rb.velocity.y);
+                    anim.SetBool("Idle", false);
+                }
+
+                if (Vector2.Distance(transform.position, pointA.position) < 0.2f)
+                {
+                    if (waiter)
+                    {
+                        StartCoroutine(Waiting());
+                    }
+
+                    Flip();
+                    moveToA = false;
+                    moveToB = true;
+                }
+            }
+            if (moveToB)
+            {
+                if (!isWaiting)
+                {
+                    rb.velocity = new Vector2(-speed * Time.deltaTime, rb.velocity.y);
+                    anim.SetBool("Idle", false);
+                }
+
+
+                if (Vector2.Distance(transform.position, pointB.position) < 0.2f)
+                {
+                    if (waiter)
+                    {
+                        StartCoroutine(Waiting());
+                    }
+
+                    Flip();
+                    moveToA = true;
+                    moveToB = false;
+                }
+            }
+        }
     }
+
+    IEnumerator Waiting()
+    {
+        anim.SetBool("Idle", true);
+        isWaiting = true;
+        Flip();
+        yield return new WaitForSeconds(waitTime);
+        isWaiting = false;
+        anim.SetBool("Idle", false);
+        Flip();
+    }
+
+    public void Flip()
+    {
+        isWalkingRight = !isWalkingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 }

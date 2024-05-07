53ba124267e5395c1ad1266628f04993eac43e04
Efef-e <emirhanefe458@gmail.com>
2024-05-08 01:18:39 +0300

    NinjaStar Bank

---
+++ Assets/Scripts/Managers/StarBank.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarBank : MonoBehaviour
+{
+
+    public int bankStar;
+    public Text bankText;
+
+    public static StarBank instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        bankText.text = "x" + bankStar.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Collect(int starCollected)
+    {
+        bankStar += starCollected;
+        bankText.text = "x" + bankStar.ToString();
+    }
+}
